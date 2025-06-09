ae4001ee802729e5b1223b4e89e142c47cb0e924
Sandra Bastos <sandra.bastos@alura.com.br>
2025-06-09 20:42:15 -0300

    adds new class

---
+++ dist/js/types/Armazenador.js
@@ -0,0 +1,20 @@
+export class Armazenador {
+    constructor() { }
+    static salvar(chave, valor) {
+        const valorComoString = JSON.stringify(valor);
+        localStorage.setItem(chave, valorComoString);
+    }
+    static obter(chave, reviver) {
+        const valor = localStorage.getItem(chave);
+        if (valor === null) {
+            return null;
+        }
+        if (reviver) {
+            return JSON.parse(valor, reviver);
+        }
+        else {
+            return JSON.parse(valor);
+        }
+        return JSON.parse(valor);
+    }
+}
